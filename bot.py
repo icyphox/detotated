fd6e76ff40d589948d1c0383ac1660af3d78957d
Akshay <nerdypepper@tuta.io>
2019-11-02 14:29:23 +0530

    add python eval command!

--- bot.py
+++ bot.py
@@ -66,6 +66,39 @@ def findurls(message):
     return urls
 
 
+def raw(text):
+    """Returns a raw string representation of text"""
+    escape_dict={'\a':r'\a',
+               '\b':r'\b',
+               '\c':r'\c',
+               '\f':r'\f',
+               '\n':r'\n',
+               '\r':r'\r',
+               '\t':r'\t',
+               '\v':r'\v',
+               '\'':r'\'',
+               '\"':r'\"',
+               '\0':r'\0',
+               '\1':r'\1',
+               '\2':r'\2',
+               '\3':r'\3',
+               '\4':r'\4',
+               '\5':r'\5',
+               '\6':r'\6',
+               '\7':r'\7',
+               '\8':r'\8',
+               '\9':r'\9'}
+    new_string=''
+    for char in text:
+        try: new_string+=escape_dict[char]
+        except KeyError: new_string+=char
+    return new_string
+
+
+def eval_msg(message):
+    sendmsg(eval(raw(message)))
+
+
 if __name__ == "__main__":
     try:
         while True:
@@ -102,6 +135,8 @@ if __name__ == "__main__":
                     sendmsg(f"sup mah nigatoni {username}!")
                 if message[:3].find(".np") != -1:
                     lastfm(username)
+                if message[:5].find(".eval") != -1:
+                    eval_msg(message[6:])
                 urls = findurls(ircmsg)
                 for u in urls:
                     page = metadata_parser.MetadataParser(url=u, search_head_only=False)
