dd92efb47ef47b7c1eb9ff5d419db42c07383d17
Akshay <nerdypepper@tuta.io>
2019-11-02 14:34:27 +0530

    sigh

--- bot.py
+++ bot.py
@@ -66,37 +66,8 @@ def findurls(message):
     return urls
 
 
-def raw(text):
-    """Returns a raw string representation of text"""
-    escape_dict={'\a':r'\a',
-               '\b':r'\b',
-               '\c':r'\c',
-               '\f':r'\f',
-               '\n':r'\n',
-               '\r':r'\r',
-               '\t':r'\t',
-               '\v':r'\v',
-               '\'':r'\'',
-               '\"':r'\"',
-               '\0':r'\0',
-               '\1':r'\1',
-               '\2':r'\2',
-               '\3':r'\3',
-               '\4':r'\4',
-               '\5':r'\5',
-               '\6':r'\6',
-               '\7':r'\7',
-               '\8':r'\8',
-               '\9':r'\9'}
-    new_string=''
-    for char in text:
-        try: new_string+=escape_dict[char]
-        except KeyError: new_string+=char
-    return new_string
-
-
 def eval_msg(message):
-    sendmsg(eval(raw(message)))
+    sendmsg(eval(message))
 
 
 if __name__ == "__main__":
